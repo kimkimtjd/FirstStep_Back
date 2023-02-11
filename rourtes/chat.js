45628b558bb7e17fba1317a9593c09031c28d2ea
UglystoneDeveloper <kimeende@naver.com>
2023-02-11 13:38:45 +0900

    tesgt

--- rourtes/chat.js
+++ rourtes/chat.js
@@ -47,6 +47,46 @@ router.get('/info/:receiver/:sender', cors(), urlencodedParser, function (req, r
 
 });
 
+//  일정잡기
+router.post('/schedule', cors(), urlencodedParser, function (req, res) {
+    const menti = req.body.id;
+    const mento = req.body.mento;
+    const schedule = req.body.schedule;
+    // const timeset = req.body.timeset;
+
+    db.mysql.query("UPDATE Consulting_Process SET schedule = ?  WHERE mentIr_id = ? AND LOCATE( ? , mentor_id) > 0 AND schedule = '' " ,
+    [schedule, menti , mento ]  , function (err, result) {
+       
+        // res.json(rows)            
+
+        if (err) throw err;
+        
+        res.json({result: 'success'})            
+    
+      });
+
+});
+
+// 후기기록
+router.post('/Review', cors(), urlencodedParser, function (req, res) {
+    const menti = req.body.id;
+    const mento = req.body.mento;
+    const schedule = req.body.schedule;
+    // const timeset = req.body.timeset;
+
+    db.mysql.query("UPDATE Consulting_Process SET Review = ?  WHERE mentIr_id = ? AND LOCATE( ? , mentor_id) > 0 AND schedule = '' " ,
+    [schedule, menti , mento ]  , function (err, result) {
+       
+        // res.json(rows)            
+
+        if (err) throw err;
+        
+        res.json({result: 'success'})            
+    
+      });
+
+});
+
 
 
 module.exports = router;
